bfeb08e17c4fb2410e8ec9cdf051ba30c704d16e
Rodrigo <jorge.caceros@gmail.com>
2022-03-21 00:51:56 -0600

    P1f1v0.1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 import React from 'react';
+import {BrowserRouter as Router,
+  Route,
+Redirect,
+Switch} from 'react-router-dom';
+import { Component } from 'react'
+import Select from 'react-select'
+import { useState } from 'react';
 import NavBar from './components/NavBar';
 
 
 
-function App() {
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const MyComponent = () => (
+  <Select options={options} />
+)
+
+export default function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
   return (
     <div className="App">
-      <NavBar />
+<NavBar/>
     </div>
-      
   );
 }
-
-export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,22 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
+import MainHeader from './MainHeader';
+
 
 function NavBar() {
   return (
-    <>
-        <nav className="NavBar">
-            <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                TRVL
-            </Link>
-            </div>
-        </nav>
-    </>
+<MainHeader>
+            <button className="main-navigation_menu-btn">
+                <span />
+                <span />
+                <span />
+
+            </button>
+
+            <h1 className="main-navigation_title">
+                <Link to="/" exact>Ejercicio 1 </Link>
+            </h1>
+            </MainHeader>
   )
 }
 
