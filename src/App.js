40492985b628f8bb4ac5e0a0c7ddbf13b9e268b0
Rodrigo <jorge.caceros@gmail.com>
2022-03-21 20:29:55 -0600

    Proyect1v1.0

--- src/App.js
+++ src/App.js
@@ -10,11 +10,16 @@ import { useState } from 'react';
 import NavBar from './components/NavBar';
 
 
-
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: 'Zona 1', label: 'Zona1' },
+  { value: 'Zona 2', label: 'Zona2' },
+  { value: 'Zona 3', label: 'Zona3' },
+  { value: 'Zona 4', label: 'Zona4' },
+  { value: 'Zona 5', label: 'Zona5' },
+  { value: 'Zona 6', label: 'Zona6' },
+  { value: 'Zona 7', label: 'Zona7' },
+  { value: 'Zona 10', label: 'Zona10' },
+  { value: 'Zona 16', label: 'Zona16' }
 ]
 
 const MyComponent = () => (
@@ -26,7 +31,27 @@ export default function App() {
 
   return (
     <div className="App">
-<NavBar/>
+      <NavBar>
+      </NavBar>
+<table className = 'tablas'>
+  <tr>
+    <td>
+    <Select className='Selectin'
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+      />
+    </td>
+
+  </tr>
+  <tr>
+    <button className='buton1'>
+      Escoger esta Zona
+    </button>
+  </tr>
+</table>
     </div>
+
+
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,15 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 import MainHeader from './MainHeader';
+import './NavBar.css';
 
 
-function NavBar() {
+const NavBar = (props) => {
   return (
 <MainHeader>
-            <button className="main-navigation_menu-btn">
-                <span />
-                <span />
-                <span />
-
-            </button>
-
-            <h1 className="main-navigation_title">
-                <Link to="/" exact>Ejercicio 1 </Link>
-            </h1>
+ <h1 className='Yo'>                                               SELECCIONE ZONA</h1>
             </MainHeader>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
